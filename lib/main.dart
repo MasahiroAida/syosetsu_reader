b87a789da97a1eb17d02ea02aed92b9ca69a95f3
MasahiroAida <mas4h1ro0106@gmail.com>
2025-06-06 13:47:07 +0900

    Switch database operations to Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'models/novel.dart';
+import 'models/bookmark.dart';
+import 'models/reading_history.dart';
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(NovelAdapter());
+  Hive.registerAdapter(BookmarkAdapter());
+  Hive.registerAdapter(ReadingHistoryAdapter());
+  await Hive.openBox<Novel>('novels');
+  await Hive.openBox<Bookmark>('bookmarks');
+  await Hive.openBox<ReadingHistory>('reading_history');
   runApp(const NovelReaderApp());
 }
